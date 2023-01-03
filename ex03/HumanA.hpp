43d25f450f5b7412566e54d582cf1e6ef159a546
Angelinne <kayumi85@hotmail.com>
2023-01-03 21:56:52 +0900

    update

---
+++ ex03/HumanA.hpp
@@ -0,0 +1,15 @@
+#ifndef HUMANA_HPP 
+#define HUMANA_HPP
+
+#include "Weapon.hpp"
+
+class HumanA
+{
+private:
+	/* data */
+public:
+	HumanA(/* args */);
+	~HumanA();
+};
+
+#endif
---
+++ ex03/main.cpp
@@ -0,0 +1,9 @@
+#include "Weapon.hpp"
+
+int main()
+{
+	Weapon s;
+
+	s.setType("ayumi");
+	std::cout << s.getType() << std::endl;
+}
